619688c34fc79f90290f36ce40aac303fe2817e7
kburger <burger.github@gmail.com>
2023-03-09 11:07:27 +0100

    Add poll endpoints.

--- dispatch.py
+++ dispatch.py
@@ -3,19 +3,22 @@ from uuid import uuid4
 
 def dispatch():
     run_uuid = str(uuid4())
-    job_uuid = str(uuid4())
     base = 'https://handler.twoc.semlab-leiden.nl'
 
     # see https://github.com/FAIRDataTeam/FAIRDataStation/blob/develop/src/main/java/org/fairdatatrain/fairdatastation/api/dto/event/train/TrainDispatchPayloadDTO.java
     payload = {
-        'jobUuid': job_uuid,
+        'jobUuid': run_uuid,
         'secret': 'foo',
-        'callbackEventLocation': f'{base}/runs/{run_uuid}/jobs/{job_uuid}/events',
-        'callbackArtifactLocation': f'{base}/runs/{run_uuid}/jobs/{job_uuid}/artifacts',
+        'callbackEventLocation': f'{base}/runs/{run_uuid}/events',
+        'callbackArtifactLocation': f'{base}/runs/{run_uuid}/artifacts',
         'trainUri': 'https://raw.githubusercontent.com/MarekSuchanek/fds-dummy-trains/main/trains/sparql/sparql-train-dashboard.ttl'
     }
 
-    print(f'posting train dispatch with run id {run_uuid} and job id {job_uuid}')
+    print(f'posting train dispatch with run id {run_uuid}')
+
+    print(f'curl {base}/runs/{run_uuid}/poll/events')
+    print(f'curl {base}/runs/{run_uuid}/poll/artifacts')
+
     r = requests.post('https://pgo-2.c4y.fairdatatrain.org/trains', json=payload)
     print(f'response status {r.status_code}, body {r.json()}')
 
--- server.py
+++ server.py
@@ -1,34 +1,39 @@
 from flask import Flask, request
-from base64 import b64decode
 
 app = Flask(__name__)
 app.logger.setLevel('INFO')
 
-@app.route('/runs/<uuid:run_uuid>/jobs/<uuid:job_uuid>/events', methods=['POST'])
-def receive_job_event(run_uuid: str, job_uuid: str) -> dict:
+events = {}
+artifacts = {}
+
+@app.route('/runs/<uuid:run_uuid>/events', methods=['POST'])
+def receive_job_event(run_uuid: str) -> dict:
     # see https://github.com/FAIRDataTeam/TrainHandler-server/blob/develop/src/main/java/org/fairdatatrain/trainhandler/api/dto/job/JobEventCreateDTO.java
-    body = request.json
-    #body['resultStatus']
-    #body['message']
-    #body['occuredAt']
-    #body['remoteId']
-    #body['secret']
-    app.logger.info(f'got event: {request.json}')
+    
+    if run_uuid not in events:
+        events[run_uuid] = []
+    events[run_uuid].append(request.json)
+
     return request.json
 
-@app.route('/runs/<uuid:run_uuid>/jobs/<uuid:job_uuid>/artifacts', methods=['POST'])
-def receive_job_artifact(run_uuid: str, job_uuid: str) -> dict:
+@app.route('/runs/<uuid:run_uuid>/poll/events', methods=['GET'])
+def poll_run_events(run_uuid: str) -> [dict]:
+    if run_uuid in events:
+        return events[run_uuid]
+    else:
+        return [], 404
+
+@app.route('/runs/<uuid:run_uuid>/artifacts', methods=['POST'])
+def receive_job_artifact(run_uuid: str) -> dict:
     # see https://github.com/FAIRDataTeam/TrainHandler-server/blob/develop/src/main/java/org/fairdatatrain/trainhandler/api/dto/job/JobArtifactCreateDTO.java
-    body = request.json
-    #body['displayName']
-    #body['fileName']
-    #body['hash']
-    #body['bytesize']
-    #body['contentType']
-    #body['occuredAt']
-    #body['remoteId']
-    #body['secret']
-    #body['base64data']
-    app.logger.info(f'got artifact: {request.json}')
-    app.logger.info(b64decode(body['base64data']))
+    
+    artifacts[run_uuid] = request.json
+    
     return request.json
+
+@app.route('/runs/<uuid:run_uuid>/poll/artifacts', methods=['GET'])
+def poll_run_artifact(run_uuid: str) -> dict:
+    if run_uuid in artifacts:
+        return artifacts[run_uuid]
+    else:
+        return {}, 404
